1f0acb72ff52ad84c5133e3d2808855986ed168c
SkippyNZ <60800666+SkippyNZ@users.noreply.github.com>
2020-07-06 07:26:18 +1200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<title> NZDF Demo </title>
+<body>
+<h1>
+<center>
+<IMG SRC="cat.jpg" ALT="Awesome"><br>
+<?php
+$eip = file_get_contents('http://169.254.169.254/latest/meta-data/public-ipv4');
+echo $eip;
+?>
+</center>
+</h1>
+</body>
+</html>
